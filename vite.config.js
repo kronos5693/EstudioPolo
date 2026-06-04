9fc8a0ad8e79c781bc25c6a74834b024c46aa407
Werner Krull <w.krull@hotmail.com>
2026-06-04 11:45:13 -0300

    correcciones

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.webp'],
-  "base": "https://kronos5693.github.io/EstudioPolo",
+ base: '/EstudioPolo/',
 });
